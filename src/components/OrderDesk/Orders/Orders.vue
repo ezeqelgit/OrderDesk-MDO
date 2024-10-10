77cd17276ee7772cdc6c8b693ceab7a3f77e8a81
ezeQel <ezeqelic@icloud.com>
2024-10-10 16:07:07 +0700

    create list orders

--- src/components/OrderDesk/Orders/Orders.vue
+++ src/components/OrderDesk/Orders/Orders.vue
@@ -20,11 +20,7 @@
           </div>
           <div class="serachItem">
             <div class="cellInput">
-              <input 
-                type="text" 
-                placeholder="Дом"
-                v-model="searchTerm"
-                @input="fetchPremises">
+              <input type="text" placeholder="Дом" v-model="searchTerm" @input="fetchPremises">
               <div class="clickSearch" @click="toggleShowList" :class="{ rotated: isListVisible }">
                 <img :src="images['downoutline']">
               </div>
@@ -37,8 +33,79 @@
             </div>
           </div>
         </div>
-        <div class="listItems"></div>
-          
+        <div class="listItems">
+          <div class="nameTable">
+            <div class="number">
+              <span>№</span>
+              <div class="hint">
+                <img :src="images['hint']">
+              </div>
+            </div>
+            <div class="created">
+              <span>Создана</span>
+              <div class="hint">
+                <img :src="images['hintblack']">
+              </div>
+            </div>
+            <div class="address">
+              <span>Адрес</span>
+            </div>
+            <div class="applicant">
+              <span>Заявитель</span>
+            </div>
+            <div class="description">
+              <span>Описание</span>
+            </div>
+            <div class="term">
+              <span>Срок</span>
+            </div>
+            <div class="status">
+              <span>Статус</span>
+              <div class="hint">
+                <img :src="images['hintblack']">
+              </div>
+            </div>
+          </div>
+          <div class="separator"></div>
+          <div v-for="appeal in appeals" :key="appeal.id" class="tableListOrders">
+            <div class="nameTable">
+              <div class="number">
+                <div class="hint">
+                  <span>{{ appeal.number }}</span>
+                </div>
+              </div>
+              <div class="created">
+                <span>{{ formatDate(appeal.created_at) }}</span>
+              </div>
+              <div class="address">
+                <span>    
+                  {{ appeal.premise ? appeal.premise.address : 'Адрес не указан' }} 
+                  {{ appeal.apartment ? appeal.apartment.label : '' }}
+                </span>
+              </div>
+              <div class="applicant">
+                <span>    
+                  {{ appeal.applicant.last_name }}
+                  {{ appeal.applicant.first_name.charAt(0) }}.
+                  <template v-if="appeal.applicant.patronymic_name">
+                    {{ appeal.applicant.patronymic_name.charAt(0) }}.
+                  </template>
+                </span>
+              </div>
+              <div class="description">
+                <span>{{ appeal.description }}</span>
+              </div>
+              <div class="term">
+                <span>{{ formatDateTime(appeal.due_date) }}</span>
+              </div>
+              <div class="status">
+                <span>{{ appeal.status.name }}</span>
+              </div>
+            </div>
+            <div class="separator"></div>
+          </div>
+        </div>
+        <div class="nextListing"></div>
       </div>
     </div>
   </div>
--- src/components/OrderDesk/Orders/data.js
+++ src/components/OrderDesk/Orders/data.js
@@ -12,11 +12,13 @@ export default Vue.extend({
       premises: [],
       token: localStorage.getItem("authToken") || "",
       searchTerm: "",
-      premiseId: ""
+      premiseId: "" ,
+      appeals: [],
     }
   },
   created() {
     this.fetchPremises();
+    this.fetchAppeals();
   },
   methods: {
     toggleShowList() {
@@ -41,6 +43,50 @@ export default Vue.extend({
       } catch (error) {
         console.error("Ошибка при получении списка домов", error.response ? error.response.data : error);
       }
+    },
+    async fetchAppeals() {
+      try {
+        const response = await axios.get("https://dev.moydomonline.ru/api/appeals/v1.0/appeals/", {
+          headers: {
+            Authorization: `Token ${this.token}`
+          }
+        });
+        this.appeals = response.data.results;
+        console.log(response.data)
+      } catch (error) {
+        console.error("Ошибка при получении списка обращений", error.response ? error.response.data : error);
+      }
+    },
+    formatDate(dateString) {
+      const date = new Date(dateString);
+      return date.toLocaleDateString('ru-RU', {
+        day: '2-digit',
+        month: '2-digit',
+        year: 'numeric'
+      });
+    },
+    formatDateTime(dateString) {
+      const date = new Date(dateString);
+      const formattedDate = date.toLocaleDateString('ru-RU', {
+        day: '2-digit',
+        month: '2-digit',
+        year: 'numeric'
+      });
+      const formattedTime = date.toLocaleTimeString('ru-RU', {
+        hour: '2-digit',
+        minute: '2-digit'
+      });
+      return `${formattedDate} ${formattedTime}`;
+    },
+    formatAddress(premise, apartmentLabel) {
+      if (premise && apartmentLabel) {
+        return `${premise} ${apartmentLabel}`;
+      } else if (premise) {
+        return premise;
+      } else if (apartmentLabel) {
+        return apartmentLabel;
+      }
+      return '';  
     }
   }
 })
